e6e33eccd204f2ee3306a30c023fc778dedc3641
devHork <141581365+devHork@users.noreply.github.com>
2023-08-06 22:19:54 +0300

    update

---
+++ chapter1.js
@@ -0,0 +1,9 @@
+var title = document.getElementById("title");
+var username = document.getElementById("username");
+
+username = "devHut";
+
+alert("THIS BLOG IS NOT AVALIBLE ON MOBILE!")
+alert("Welcome to my blog." +" "+ "(Develop by "+username+")");
+console.log(username);
+console.log("why you checking the console?")
